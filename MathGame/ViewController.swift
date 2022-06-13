4eaf1834050bfc8b87a2725da2464d2c2cda8691
Yoga <jc051533@gmail.com>
2022-06-13 12:08:24 +0800

    Initial Commit

---
+++ MathGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MathGame
+//
+//  Created by 王鈞則 on 2022/6/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
